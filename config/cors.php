8e1e449fa27aca039c09b152f5ddc308d1d1d943
ghawra <ghauraaja@gmail.com>
2026-06-03 09:57:53 +0700

    fixed swagger

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'docs/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'docs/*', 'docs'],
 
     'allowed_methods' => ['*'],
 
